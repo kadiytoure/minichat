3d1522ceac8992ad0a025bc6b2d0acf107a2835d
toure <tourekadiy@gmail.com>
2017-09-25 20:00:55 +0200

    creation of form;

--- minichat.php
+++ minichat.php
@@ -1,2 +1,18 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="minichat_post.php" method="post">
+    <label for="pseudo">Pseudo:</label>
+    <input type="text" id="idpseudo" name="pseudo" placeholder="enter your name"/>
+    <label for="message">Message:</label>
+    <input type="text" id="idmessage" name="message" placeholder="enter your message"/>
+    <input type="submit" value="send"/>
+    </form>
+</body>
+</html>
