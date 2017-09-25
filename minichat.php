ef6e447938a8d3a2c28e36666309881dbfbe8eaf
toure <tourekadiy@gmail.com>
2017-09-25 20:21:01 +0200

    update form;

--- minichat.php
+++ minichat.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
     <form action="minichat_post.php" method="post">
-    <label for="pseudo">Pseudo:</label>
-    <input type="text" id="idpseudo" name="pseudo" placeholder="enter your name"/>
-    <label for="message">Message:</label>
-    <input type="text" id="idmessage" name="message" placeholder="enter your message"/>
-    <input type="submit" value="send"/>
+        <label for="pseudo">Pseudo:</label>
+        <input type="text" id="idpseudo" name="pseudo" placeholder="enter your name" /><br/>
+        <label for="message">Message:</label>
+        <input type="text" id="idmessage" name="message" placeholder="enter your message" /><br/>
+        <input type="submit" value="send" /><br/>
     </form>
 </body>
+
 </html>
