baff9d75b2026a16ac5bb89d06621cd965e24b3e
toure <tourekadiy@gmail.com>
2018-01-23 16:32:33 +0100

    condition if;

--- minichat_post.php
+++ minichat_post.php
@@ -1,9 +1,16 @@
 <?php
 // Effectuer requête insérant le message;
 // Rediriger vers minichat.php
-header('Location: minichat.php');
+
 //récupération des valeurs du formulaire
 $pseudo = htmlspecialchars ($_POST['pseudo']);
 $message = htmlspecialchars ($_POST['message']);
+
+if (empty($_POST['message'] AND $_POST['pseudo'])){
+    http_response_code(400);
+    header('Content-Type: text/plain');
+    echo 'expect a message parameter';
+}
+header('Location: minichat.php');
 
 ?>
