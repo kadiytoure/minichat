ecbac707a8bf8e2c826c569b7fd16626a530842a
toure <tourekadiy@gmail.com>
2018-01-23 16:23:05 +0100

    récupération des valeurs du formulaire;

--- minichat_post.php
+++ minichat_post.php
@@ -2,4 +2,8 @@
 // Effectuer requête insérant le message;
 // Rediriger vers minichat.php
 header('Location: minichat.php');
+//récupération des valeurs du formulaire
+$pseudo = htmlspecialchars ($_POST['pseudo']);
+$message = htmlspecialchars ($_POST['message']);
+
 ?>
