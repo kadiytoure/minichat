6ee07bd28f6ce90a168eae566739daaff33e8bf5
toure <tourekadiy@gmail.com>
2018-01-23 17:12:11 +0100

    requêtes PDO et BDD;

--- minichat_post.php
+++ minichat_post.php
@@ -10,9 +10,31 @@ if (empty($_POST['message'] AND $_POST['pseudo'])){
     http_response_code(400);
     header('Content-Type: text/plain');
     echo 'expect a message parameter';
+    exit(1);
 }
 header('Location: minichat.php');
 
+try {
 $bdd = new PDO('mysql:host=localhost; dbname=minichat;charset=utf-8', 'kadiy', 'kadiy');
+}
+catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+/*
+$reponse = $bdd->query('SELECT * FROM chat LIMIT 0, 10');
+$donnees = $reponse->fetch();
+while ($donnees = $reponse->fetch()) {
 
+}
+
+$reponse->closeCursor();
+*/
+$req = $bdd->prepare('INSERT INTO chat(`id`, `pseudo`, `message`) VALUES(:id, :pseudo, :message)');
+$req->execute(array(
+    'id' => $id,
+    'pseudo' => $pseudo,
+    'message' => $message
+));
+
+echo 'Le message a bien été ajouté!';
 ?>
